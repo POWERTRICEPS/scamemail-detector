688c5ac5a15802721df2da55abc70d0c33260a5e
Raymond <imnotdacoco05@gmail.com>
2025-08-19 23:08:11 -0700

    Added react
    - Added helper functions for the API
    - Added simple frontned UI allowing users to input emails
    - Added UI so users can see the response from model

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+
+  const [email, setEmail] = useState("");
+  const [result, setResult] = useState(null);
+  
+
+  const handleSubmit = async () => {
+    const response = await fetch("http://localhost:5000/api/predict", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ body: email }),
+    });
+
+    const data = await response.json();
+    setResult(data);
+  };
+
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+    style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh"
+    }}
+  >
+    <div style={{ textAlign: "center" }}>
+      <h1>Scam Email Detector</h1>
+      <textarea
+        rows="20"
+        cols="100"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Paste your email text here..."
+      />
+      <br />
+      <button onClick={handleSubmit}>Check Email</button>
+
+      {result && (
+        <div style={{ marginTop: "20px" }}>
+          <h2>Result: {result.label}</h2>
+          <p>
+            Scam Probability: {(result.scam_probability * 100).toFixed(0)}%
+          </p>
+        </div>
+      )}
     </div>
-  );
+  </div>
+);
 }
+
 
 export default App;
